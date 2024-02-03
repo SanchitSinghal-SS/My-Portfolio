b287fde81a729c8430d6807ff95409fdc99742a7
Sanchit Singhal <sanchit.singhal2811@gmail.com>
2024-02-04 01:04:50 +0530

    Changes

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -10,7 +10,7 @@ const StarWrapper = (Component, idName) =>
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.1 }}
+        viewport={{ once: true, amount: 0.15 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-5`}
       >
         <span className='hash-span' id={idName}>
